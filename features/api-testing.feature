2114c5769159778656034ad48123eff4c1ac2088
andinitah <andinita.nurul@fore.coffee>
2020-05-23 17:02:49 +0700

    fix api-tesing

--- features/api-testing.feature
+++ features/api-testing.feature
@@ -3,3 +3,8 @@ Feature: CRUD
     Scenario: Create booking using json in POST body
         When I send a POST request to "https://restful-booker.herokuapp.com/booking"
         Then the response status should be "200"
+
+    Scenario: Delete Booking
+        Given  Get token to "https://restful-booker.herokuapp.com/auth"
+        When Delete id "10" from "https://restful-booker.herokuapp.com/booking"
+        Then the response status should be "201"
--- features/step_definitions/api-testing.rb
+++ features/step_definitions/api-testing.rb
@@ -1,6 +1,17 @@
 require 'rest-client'
+require 'json'
 
-When /^I send a POST request to "(.*?)"$/ do |url|
+Given /^Get token to "([^\"]*)"$/ do |url|
+  @token_response = RestClient.post url, {
+    'username' => 'admin',
+    'password' => 'password123',
+}.to_json, {
+  :content_type => :json,
+  :accept => :json
+}
+end
+
+When /^I send a POST request to "([^\"]*)"$/ do |url|
   @resp = RestClient.post url, {
     'firstname' => 'Jim',
     'lastname' => 'Brown',
@@ -15,6 +26,18 @@ When /^I send a POST request to "(.*?)"$/ do |url|
       :content_type => :json,
       :accept =>:json }
   end
+
+When /^Delete id "([^\"]*)" from "([^\"]*)"$/ do |id, url|
+  responseToken = JSON.parse(@token_response.body)
+  cookie = "token=%s" % [responseToken['token']]
+  puts cookie
+  urlWithId = "%s/%s" % [url, id]
+  puts urlWithId
+  @resp = RestClient.delete urlWithId, {
+    :cookie => cookie,
+    :authorization => 'Basic'
+  }
+end
 
 Then /^the response status should be "([^\"]*)"$/ do |status|
   puts "response code : %d" % [@resp.code]
