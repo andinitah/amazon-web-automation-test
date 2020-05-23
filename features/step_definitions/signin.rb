d3c9ad3522fec81a6b84750f723763baa95ca1a4
andinitah <andinita.nurul@fore.coffee>
2020-05-23 17:05:03 +0700

    take the comment sign

--- features/step_definitions/signin.rb
+++ features/step_definitions/signin.rb
@@ -1,28 +1,28 @@
-# require 'selenium-webdriver'
+require 'selenium-webdriver'
 
-# driver = Selenium::WebDriver.for:firefox
+driver = Selenium::WebDriver.for:firefox
 
-# Given /^Open "(.*?)"$/ do |site|
-#   driver.get site
-# end
+Given /^Open "(.*?)"$/ do |site|
+  driver.get site
+end
 
-# And /^Go to SignIn page$/ do
-#   driver.find_element(:xpath,'//span[contains(text(),"Hello, Sign in")]').click
-#   p "Page title is #{driver.title}"
-# end
+And /^Go to SignIn page$/ do
+  driver.find_element(:xpath,'//span[contains(text(),"Hello, Sign in")]').click
+  p "Page title is #{driver.title}"
+end
 
-# When /^User fill the field with "(.*?)"$/ do |account|
-#   driver.find_element(:id,'ap_email').click
-#   driver.find_element(:id,'ap_email').send_keys account
-#   driver.find_element(:id,'continue').click
-#   sleep(2)
-# end
+When /^User fill the field with "(.*?)"$/ do |account|
+  driver.find_element(:id,'ap_email').click
+  driver.find_element(:id,'ap_email').send_keys account
+  driver.find_element(:id,'continue').click
+  sleep(2)
+end
 
-# Then /^Alert error "(.*)" appears$/ do |errormsg|
-#   confirm = driver.find_element(:xpath, '//span[@class="a-list-item"]')
-#   confirm.text.include? errormsg
-# end
+Then /^Alert error "(.*)" appears$/ do |errormsg|
+  confirm = driver.find_element(:xpath, '//span[@class="a-list-item"]')
+  confirm.text.include? errormsg
+end
 
-# And /^Take screenshot "(.*?)"$/ do |validation|
-#     driver.save_screenshot (validation)
-# end
+And /^Take screenshot "(.*?)"$/ do |validation|
+    driver.save_screenshot (validation)
+end
